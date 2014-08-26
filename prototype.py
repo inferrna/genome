5baccd4eaf18952a020cdf8a8fa0ca83c4fa1837
inferrna <inferrna@gmail.com>
2014-08-26 12:46:19 +0900

    +variable equation size

--- prototype.py
+++ prototype.py
@@ -15,40 +15,57 @@ def dec2str(num):
 
 print( cl.get_cl_header_version() )
 
-result = 3.0
+result = 1.0
 #results = [3.0, 5.0, 2.0]
 #ivalues = [[1.0, 2.0, 3.0, 4.0], [1.0, 3.0, 1.0, 2.0], [4.0, 1.0, 1.0, 2.0]]
 coeffs = np.random.random(9)
-equation = "(a_g[gid]+2*b_g[gid])*(3*c_g[gid]+4*d_g[gid]) - "+str(result)+""
 #eqlst = ["a_g[gid]"*str(iv[0])
 #exit()
-nvars = 4
+ninpt = 1
+nvars = 400
 nsamp = 500
 varnames = []
 valnames = []
 for i in range(0, nvars):
     varnames.append(dec2str(i)+"_gnm")
     valnames.append(dec2str(i)+"_val")
-
+varsofid = [var+'[gid]' for var in varnames]
+valsof_i = [val+'[i]' for val in valnames]
+varsprms = ['__global const float *'+var for var in varnames]
+valsprms = ['__global const float *'+val for val in valnames]
+eq = ['*'.join(c) for c in zip(varsofid, valsof_i)]
+equation = '+'.join(eq)+" - "+str(result)+""
+varspstr = ', '.join(varsprms+valsprms)
+print(varsofid)
+print(varsprms)
 print(varnames)
 print(valnames)
-
-
+print(eq)
+print(equation)
+print(varspstr)
+#exit()
 arr_np = np.random.rand(nvars*nsamp).astype(np.float32) - np.random.rand(nvars*nsamp).astype(np.float32)
+inp_np = np.random.rand(nvars*ninpt).astype(np.float32) - np.random.rand(nvars*ninpt).astype(np.float32)
 
 ctx = cl.create_some_context()
 queue = cl.CommandQueue(ctx)
 
 arr4np = arr_np.reshape(nvars, nsamp)
+inp4np = inp_np.reshape(nvars, ninpt)
 
 mf = cl.mem_flags
 run = cl.Program(ctx, """
-__kernel void sum(__global const float *a_g, __global const float *b_g, __global const float *c_g, __global const float *d_g, __global float *res_g) {
+__kernel void sum("""+varspstr+""", __global float *res_g) {
   int gid = get_global_id(0);
-  res_g[gid] = """+equation+""";
+  float _res = 0.0;
+  for(int i=0; i<"""+str(ninpt)+"""; i++){
+    _res += fabs("""+equation+""");
+    //_res += """+equation+""";
+  }
+  res_g[gid] = _res;
 }
 """).build()
-arrs_g = [[]]*(nvars+1)
+arrs_g = [[]]*(2*nvars+1)
 res_g = cl.Buffer(ctx, mf.WRITE_ONLY, arr4np[0].nbytes)
 res_np = np.empty_like(arr4np[0])
 _arr4np = [[0]]*nvars
@@ -56,7 +73,7 @@ global_offset = None
 g_times_l = False
 wait_for = None
 
-for cy in range(0, 130):
+for cy in range(0, 13000):
    # if cy>0 and cl.enqueue_fill_buffer:
    #     cl.enqueue_fill_buffer(queue, a_g, arr4np[0], 0, nsamp)
    #     cl.enqueue_fill_buffer(queue, b_g, arr4np[1], 0, nsamp)
@@ -64,6 +81,7 @@ for cy in range(0, 130):
    #     cl.enqueue_fill_buffer(queue, d_g, arr4np[3], 0, nsamp)
     for i in range(0, nvars):
         arrs_g[i] = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=arr4np[i])
+        arrs_g[i+nvars] = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=inp4np[i])
     arrs_g[-1] = res_g
     #run.sum.set_args(*arrs_g)
     #cl.enqueue_nd_range_kernel(queue, run.sum, arr4np[i].shape, None, global_offset, wait_for, g_times_l=g_times_l)
@@ -101,7 +119,7 @@ for cy in range(0, 130):
             _arr4np[idx][i] = too
             _arr4np[idx][-(i+1)] = frm
     print("Newlen == ", len(_arr4np[0]))
-    d1 = np.array([[0]*diff]*4)
+    d1 = np.array([[0]*diff]*nvars)
     for i in range(0, diff):
         r = random.randint(0, nwlen-1)
         for j in range(0, len(d1)):
