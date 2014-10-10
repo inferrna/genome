adf441fbb9e06b87ba3b5a9ab2909ec11033e881
inferrna <inferrna@gmail.com>
2014-10-10 18:34:32 +0900

    +samples loop

--- testn.py
+++ testn.py
@@ -3,10 +3,8 @@ dm = 1
 counters = "ijkzhiuy"
 sums = "abcdef"
 lneurons = ["lnr", "dnr"]
-a = np.array([5, 3, 2, 1], dtype=np.uint32)
-s = []
-s.append("float sum = 0.0;")
-s.append("float sums[{0}];".format(a.sum()))
+a = np.array([7, 5, 3, 2, 1], dtype=np.uint32)
+samples = 5
 #s.append("for({0}=0; {0}<{1}; {0}++)".format(counters[dm], str(len(a)))+"{ //\"Layers\" loop")        #Layers loop
 currner = 0
 nextner = 0
@@ -14,11 +12,16 @@ currcon = 0
 n = 0
 #nextner += a[n]
 #s.append(dm*"\t"+"sums[{1}][{0}]+=xxx;".format(counters[dm], dm-1))
+s = []
+s.append(dm*"\t"+"float dnr[{0}];".format(a[1]))
+s.append(dm*"\t"+"float lnr[{0}];".format(a[1]))
+s.append(dm*"\t"+"for(uint {0}=0; {0}<{1}*{2}; {0}+={2})".format("n_sam", samples, a[0])+"{ //\"Samples\" loop ") #"Samples" loop
+dm+=1
 s.append(dm*"\t"+"for(uint {0}=0; {0}<{1}; {0}++)".format(counters[dm], str(a[n]))+"{ //\"Neurons\" loop "+str(n)) #"Neurons" loop
 s.append((dm+1)*"\t"+"for(uint {0}=0; {0}<{1}; {0}++)".format(counters[dm+1], str(a[n+1]))+"{ //\"Connections\" loop "+str(n)) #"Connections" loop
-s.append((dm+2)*"\t"+"{6}[{1}] += gneurons[{3}]*conns[{3}*{5}+{1}]".format(\
+s.append((dm+2)*"\t"+"{6}[{1}] += gneurons[{2}+{3}]*conns[{3}*{5}+{1}]".format(\
                                                                                nextner, counters[dm+1],\
-                                                                               currner, counters[dm],\
+                                                                               "n_sam", counters[dm],\
                                                                                currcon, str(a[n+1]), 
                                                                                lneurons[n%2])+";\n"+(dm+1)*"\t"+"}") #"Connections" loop
 s.append(dm*"\t"+"}") #"Connections" loop
@@ -39,4 +42,5 @@ for n in range(1, len(a)-1):
     currcon += a[n]*a[n+1]
     currner += a[n]
     #dm+=1
+s.append((dm-1)*"\t"+"}") #"Samples" loop
 print("\n".join(s))
